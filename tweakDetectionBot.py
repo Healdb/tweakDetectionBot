db4aad7414ca9b1cf6ae8bb4a2787499d5787095
Healdb <Healdb@users.noreply.github.com>
2015-02-17 17:34:56 -0500

    checks a dictionary to see if the package is an actual word
    This cuts down spam by filtering out tweaks with names that are actual words

--- tweakDetectionBot.py
+++ tweakDetectionBot.py
@@ -2,20 +2,14 @@ import requests
 import feedparser
 import praw
 from bs4 import BeautifulSoup
-
+import enchant
+d = enchant.Dict("en_US")
 r = praw.Reddit('/r/jailbreak package description bot')
 r.login("username","password")
-
-def checkType(link):
-    response = requests.post(link)
-    soup = BeautifulSoup(response.text)
-    tag= soup.find(id="section")
-    type = str(tag.string)
-    if type == "Tweaks":
-        return True
-    else:
+            
+def checkTweak(packageName):
+    if d.check(packageName):
         return False
-def checkTweak(packageName):        
     response = requests.post("http://planet-iphones.com/cydia/feed/name/" + packageName)
     feed = feedparser.parse( response.text )
     for item in feed[ "items" ]:
@@ -23,7 +17,17 @@ def checkTweak(packageName):
         if title.lower() == packageName:
             return True
     return False
-    
+def checkType(link):
+    response = requests.post(link)
+    soup = BeautifulSoup(response.text)
+    tag= soup.find(id="section")
+    try:
+        type = str(tag.string)
+    except:
+        return False
+    if type == "Tweaks":
+        return True
+    return False
 def getTweak(packageName):        
     response = requests.post("http://planet-iphones.com/cydia/feed/name/" + packageName)
     feed = feedparser.parse( response.text )
@@ -35,6 +39,7 @@ def getTweak(packageName):
             link = "http://cydia.saurik.com/package/" + link.replace("http://planet-iphones.com/cydia/id/", "")
             if checkType(link):
                 return link, descrip
+            
 while True:
     subreddit = r.get_subreddit('jailbreak')
     for submission in subreddit.get_new(limit=25):
@@ -43,7 +48,10 @@ while True:
             title = submission.title
             for words in title.split(" "):
                 if checkTweak(words.lower()):
-                    link, descrip = getTweak(words.lower())
+                    try:
+                        link, descrip = getTweak(words.lower())
+                    except:
+                        break
                     submission.add_comment("The following is short description and link to the tweak mentioned in this post: \n\n Title: [" + words +"](" + link + ") \n\nDescription: " + descrip + " \n\nCreated by healdb. This bot uses http://planet-iphones.com to find its information, and therefore makes no guarantees on it's accuracy")
                     print "Found post and commented, link: " + submission.permalink
         obj.write(submission.id)
